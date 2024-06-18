ade8a10073482a5f09403dd2fa2af9988609c59f
darwinpiodos <darwindivinopiodos21@gmail.com>
2024-06-18 23:32:59 +0800

    CRUD Users in pagination admindarkpiodos june 18, 2024

--- src/views/Users.jsx
+++ src/views/Users.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
+import { Link } from "react-router-dom";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -14,14 +16,60 @@ const Users = () => {
       .get("/users")
       .then(({ data }) => {
         setLoading(false);
-        console.log(data);
+        setUsers(data.data);
       })
       .catch(() => {
         setLoading(false);
       });
   };
 
-  return <div>Users</div>;
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>Users</h1>
+        <Link to="/users/new" className="btn-add">
+          Add New
+        </Link>
+      </div>
+
+      <div className="card animated fadeInDown">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Create Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((u) => (
+              <tr>
+                <td>{u.id}</td>
+                <td>{u.name}</td>
+                <td>{u.email}</td>
+                <td>{u.created_at}</td>
+                <td>
+                  <Link to={"/users/" + u.id} className="btn-edit">
+                    Edit
+                  </Link>
+                  &nbsp;
+                  <button className="btn-delete">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
